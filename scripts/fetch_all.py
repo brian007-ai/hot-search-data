1ca441ce7e0965bb6cf57154a407b6b17d224ec0
FangBo <fangbo@ponovo.cn>
2026-09-22 13:16:23 +0800

    fix: 移除 bilibili (API 频繁风控封 IP)

--- scripts/fetch_all.py
+++ scripts/fetch_all.py
@@ -129,8 +129,6 @@ JSON_SOURCES = [
     ("douyin",   "https://www.iesdouyin.com/web/api/v2/hotsearch/billboard/word/",
      "douyin_parser",  "news", {"Referer": "https://www.iesdouyin.com/"}),
 
-    ("bilibili", "https://api.bilibili.com/x/web-interface/popular?ps=30",
-     "bilibili_parser", "ent", {"Referer": "https://www.bilibili.com/"}),
 
     ("weibo",    "https://weibo.com/ajax/side/hotSearch",
      "weibo_parser",  "news", {"Referer": "https://weibo.com/"}),
